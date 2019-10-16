6a2055906daa818e1dccdf2dcd994f4d97ba0ec2
Sebastian Wiercinski <s.wiercinski@gmx.de>
2019-10-16 22:06:13 +0200

    added gel

---
+++ Ising_2d/ising.h
@@ -0,0 +1,17 @@
+//
+//  ising.h
+//  Ising_2d
+//
+//  Created by Sebastian on 16.10.19.
+//
+
+#ifndef ising_h
+#define ising_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <gsl/gsl_rng.h>
+
+#endif /* ising_h */
